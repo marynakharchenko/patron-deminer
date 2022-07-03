406cd10c21e0432cca9c42876613bcd02ff59562
novokhatskyioleksii <avecezar17@gmail.com>
2022-07-03 04:09:12 +0300

    add arrows and flag icons

--- src/script.js
+++ src/script.js
@@ -5,6 +5,27 @@ window.onload = () => {
   getMinesList();
   getLevelsPopup();
 
+  document.getElementById('upArrowIcon').onclick = () => {
+    document.dispatchEvent(new KeyboardEvent('keydown', {'code': 'ArrowUp'}));
+    document.dispatchEvent(new KeyboardEvent('keyup', {'code': 'ArrowUp'}));
+  };
+  document.getElementById('downArrowIcon').onclick = () => {
+    document.dispatchEvent(new KeyboardEvent('keydown', {'code': 'ArrowDown'}));
+    document.dispatchEvent(new KeyboardEvent('keyup', {'code': 'ArrowDown'}));
+  };
+  document.getElementById('leftArrowIcon').onclick = () => {
+    document.dispatchEvent(new KeyboardEvent('keydown', {'code': 'ArrowLeft'}));
+    document.dispatchEvent(new KeyboardEvent('keyup', {'code': 'ArrowLeft'}));
+  };
+  document.getElementById('rightArrowIcon').onclick = () => {
+    document.dispatchEvent(new KeyboardEvent('keydown', {'code': 'ArrowRight'}));
+    document.dispatchEvent(new KeyboardEvent('keyup', {'code': 'ArrowRight'}));
+  };
+  document.getElementById('actionIconsFlag').onclick = () => {
+    document.dispatchEvent(new KeyboardEvent('keydown', {'code': 'Space'}));
+    document.dispatchEvent(new KeyboardEvent('keyup', {'code': 'Space'}));
+  };
+
   document.getElementById('btn-start').onclick = () => {
     document.getElementById('background-start').style.display = 'none';
     document.getElementById('collection').style.display = 'none';
