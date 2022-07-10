3ca95cfbb918c98418514b4143287bca14788180
novokhatskyioleksii <avecezar17@gmail.com>
2022-07-10 23:31:03 +0300

    level 1-3 bear moves

--- src/core/levels/level02.js
+++ src/core/levels/level02.js
@@ -34,69 +34,9 @@ const LEVEL_MAP = [
 
 const BEAR_AVAILABLE = false;
 const BEAR_SPEED = 1;
-const BEAR_STEPS = [
-  // { row: 19, col: 9 },
-  { row: 19, col: 10, direction: 'Right' },
-  { row: 19, col: 11, direction: 'Right' },
-  { row: 19, col: 12, direction: 'Right' },
-  { row: 19, col: 13, direction: 'Right' },
-  { row: 19, col: 14, direction: 'Right' },
-  { row: 18, col: 14, direction: 'Up' },
-  { row: 17, col: 14, direction: 'Up' },
-  { row: 17, col: 15, direction: 'Right' },
-  { row: 17, col: 16, direction: 'Right' },
-  { row: 16, col: 16, direction: 'Up' },
-  { row: 15, col: 16, direction: 'Up' },
-  { row: 15, col: 15, direction: 'Left' },
-  { row: 15, col: 14, direction: 'Left' },
-  { row: 15, col: 13, direction: 'Left' },
-  { row: 14, col: 13, direction: 'Up' },
-  { row: 13, col: 13, direction: 'Up' },
-  { row: 12, col: 13, direction: 'Up' },
-  { row: 11, col: 13, direction: 'Up' },
-  { row: 10, col: 13, direction: 'Up' },
-  { row: 9, col: 13, direction: 'Up' },
-  { row: 9, col: 12, direction: 'Left' },
-  { row: 9, col: 11, direction: 'Left' },
-  { row: 9, col: 10, direction: 'Left' },
-  { row: 9, col: 9, direction: 'Left' },
-  { row: 9, col: 8, direction: 'Left' },
-  { row: 9, col: 9, direction: 'Right' },
-  { row: 9, col: 10, direction: 'Right' },
-  { row: 9, col: 11, direction: 'Right' },
-  { row: 9, col: 12, direction: 'Right' },
-  { row: 9, col: 13, direction: 'Right' },
-  { row: 10, col: 13, direction: 'Down' },
-  { row: 11, col: 13, direction: 'Down' },
-  { row: 12, col: 13, direction: 'Down' },
-  { row: 13, col: 13, direction: 'Down' },
-  { row: 14, col: 13, direction: 'Down' },
-  { row: 15, col: 13, direction: 'Down' },
-  { row: 15, col: 14, direction: 'Right' },
-  { row: 15, col: 15, direction: 'Right' },
-  { row: 15, col: 16, direction: 'Right' },
-  { row: 16, col: 16, direction: 'Down' },
-  { row: 17, col: 16, direction: 'Down' },
-  { row: 17, col: 15, direction: 'Left' },
-  { row: 17, col: 14, direction: 'Left' },
-  { row: 18, col: 14, direction: 'Down' },
-  { row: 19, col: 14, direction: 'Down' },
-  { row: 19, col: 13, direction: 'Left' },
-  { row: 19, col: 12, direction: 'Left' },
-  { row: 19, col: 11, direction: 'Left' },
-  { row: 19, col: 10, direction: 'Left' },
-  { row: 19, col: 9, direction: 'Left' },
-];
+const BEAR_STEPS = [];
 
-let MINES_NUMBER = 0;
-
-for (let row = 0; row < LEVEL_MAP.length; row++) {
-  for (let col = 0; col < LEVEL_MAP[0].length; col++) {
-    if (LEVEL_MAP[row][col].includes(M)) MINES_NUMBER++;
-  }
-}
-
-const LEVEL_SECONDS = (BEAR_STEPS.length + (MINES_NUMBER * 2)) * BEAR_SPEED;
+const LEVEL_SECONDS = 120;
 
 const MINE_TYPES = [
   MINES.PTM_3.MINE_ID,
