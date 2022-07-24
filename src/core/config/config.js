3e52fbc69323485005969f52cb6e720d565268f0
novokhatskyioleksii <avecezar17@gmail.com>
2022-07-24 18:06:03 +0300

    fix backgroundColor

--- src/core/config/config.js
+++ src/core/config/config.js
@@ -1,14 +1,10 @@
-import getLevelSettings from '../constants/levels';
-
 class Config {
   static get view() {
-    const { BACKGROUND_COLOR } = getLevelSettings();
     const size = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth;
 
     return {
       width: size,
       height: size,
-      backgroundColor: BACKGROUND_COLOR || 0x729641,
       worldWidth: size * 6,
       worldHeight: size * 6,
     };
